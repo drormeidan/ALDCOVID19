5bb958275551c10082581ef0d48606c7cb67757b
drormeidan <46561233+drormeidan@users.noreply.github.com>
2020-05-02 21:08:28 +0300

    Delete SEIRindoor.m

--- SEIRindoor.m
+++
@@ -1,9 +0,0 @@
-function [ dydt ] = SEIRindoor(t,y,alpha,gamma)
-% S_LC=y(1),E_LC=y(2),I_LC=y(3),R_LC=y(4)
-dydt = zeros(4,1);
-dydt(1)=0;
-dydt(2)=-(gamma*y(2));
-dydt(3)=(gamma*y(2))-(alpha*y(3));
-dydt(4)=alpha*y(3);
-end
-
